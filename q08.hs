fb1b73df5853c78eda7cdc62c95dd64b594cfe2c
LinuxMercedes <LinuxMercedes@gmail.com>
2014-06-16 18:42:03 -0500

    Finished q 08

---
+++ q08.hs
@@ -0,0 +1,21 @@
+{-
+Eliminate consecutive duplicates of list elements.
+
+If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed.
+
+Example:
+
+* (compress '(a a a a b c c a a d e e e e))
+(A B C A D E)
+
+Example in Haskell:
+
+> compress "aaaabccaadeeee"
+"abcade"
+-}
+
+compress :: (Eq a) => [a] -> [a]
+compress (x:xs) = foldl (\acc x -> if x /= last acc then acc ++ [x] else acc ) [x] xs
+
+main = do
+	print $ compress "aaaabccaadeeee"
