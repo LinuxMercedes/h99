9118936091fd51a8acef4f12815847dda0712306
LinuxMercedes <linuxmercedes@gmail.com>
2015-02-27 14:55:33 -0600

    question 22

---
+++ q22.hs
@@ -0,0 +1,18 @@
+{- 
+	Create a list containing all integers within a given range.
+
+	Example in Haskell:
+
+	Prelude> range 4 9
+	[4,5,6,7,8,9]
+
+-}
+
+range :: Int -> Int -> [Int]
+range x y 
+	| x == y = [x]
+	| otherwise = x:(range (x+1) y)
+
+main = do 
+	print $ range 4 9
+
